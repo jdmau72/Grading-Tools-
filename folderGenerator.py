acd3a8c431c6a4b7cb3682b2d56f8235c11e1e7c
Justin Mau <98239387+jdmau72@users.noreply.github.com>
2023-02-04 21:21:54 -0700

    Updated code to use a GUI
    Instead of using command line prompts, now the programs use a tkinter GUI. Makes things a lot easier to use

--- folderGenerator.py
+++ folderGenerator.py
@@ -5,47 +5,104 @@
 # --------------
 import os
 import csv
+from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
+
+# ------------------------------------
+window = Tk()
+window.title('Folder Generator')
+
+classFolder = StringVar()
+classlistFile = StringVar()
+assignmentType = StringVar()
+assignmentCount = IntVar()
 
 
-# Main --------------------------------------------------------------------
-def main():
-    os.chdir(os.getcwd() + "/../")
-    print(os.getcwd())
 
-    className = input("Please enter the name of the folder for the class: ")
-    os.chdir(os.getcwd() + "/" + className)
-    print(os.getcwd())
+# define button click functions
+def selectClassFolder():
+    dir = filedialog.askdirectory()
+    classFolder.set(dir)
+    
+def selectClasslist():
+    file = filedialog.askopenfilename()
+    classlistFile.set(file)
+    
+def createFolders():
+    # sets the working directory to the class folder
+    os.chdir(classFolder.get())
 
     # reads in the classlist
     classlist = []
-    with open('classlist.csv', mode = 'r') as file:
+    with open(classlistFile.get(), mode = 'r') as file:
         csvFile = csv.reader(file)
         for line in csvFile:
             classlist.append((line[0] + line[1]).strip(" "))
 
-    # gets either Lab, Project, Assignment
-    assignmentType = input("Please enter the assignment type: ")
-
-    # get the number of those assignments
-    numAssignments = input("How many of these assignments?: ")
-
-
-    for i in range(int(numAssignments)):
+    for i in range(int(assignmentCount.get())):
         # creates a directory assignment
-        os.mkdir(assignmentType + str(i + 1))   
+        os.mkdir(assignmentType.get() + str(i + 1))   
         # changes directory to that new folder
-        os.chdir(os.getcwd() + f"/{assignmentType}{str(i + 1)}")
+        os.chdir(os.getcwd() + f"/{assignmentType.get()}{str(i + 1)}")
         
         # goes through the classlist and creates a folder for each student
         for student in classlist:
             os.mkdir(student)
                 
         os.chdir(os.getcwd() + "/../")
-        
-    print("All done!")
-# End of main() ----------------------------------
+    
+    # updates status and resets fields    
+    messagebox.showinfo("Message", "All done!")
+    classFolder.set("")
+    classlistFile.set("")
+    assignmentType.set("")
+    assignmentCount.set(0)
+
+    
+    
+# creating all the display elements
+label_classFolder = Label(text="Class Folder: ")
+entry_classFolderPath = Entry(window, textvariable = classFolder)
+button_explore1 = Button(window,
+                        text = "Browse Folders",
+                        command = selectClassFolder)
+
+label_classlist = Label(text="Classlist: ")
+entry_classlistFile = Entry(window, textvariable = classlistFile)
+button_explore2 = Button(window,
+                        text = "Browse Files",
+                        command = selectClasslist)
+
+label_assignmentType = Label(text="Assignment Type (Lab, Project, etc): ")
+entry_assignmentType = Entry(window, textvariable = assignmentType)
+
+label_assignmentCount = Label(text="Number of Assignments: ")
+entry_assignmentCount = Entry(window, textvariable = assignmentCount)
+
+button_createFolders = Button(window,
+                                text = "Create Folders",
+                                command = createFolders)
 
 
 
-if __name__ == "__main__":
-    main()              
+# Sets the elements to the basic grid
+label_classFolder.grid(column=1, row=1)
+entry_classFolderPath.grid(column=2, row=1)
+button_explore1.grid(column = 3, row = 1)
+
+label_classlist.grid(column=1, row=2)
+entry_classlistFile.grid(column=2, row=2)
+button_explore2.grid(column = 3, row = 2)
+
+label_assignmentType.grid(column=1, row=3)
+entry_assignmentType.grid(column=2, row=3)
+
+label_assignmentCount.grid(column=1, row=4)
+entry_assignmentCount.grid(column=2, row=4)
+
+button_createFolders.grid(column=4, row=5)
+
+
+# -----------
+window.mainloop()            
--- sortSubmissions.py
+++ sortSubmissions.py
@@ -7,6 +7,9 @@ import os
 import csv
 import shutil
 from zipfile import ZipFile
+from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
 
 
 # Main --------------------------------------------------------------------
@@ -59,6 +62,120 @@ def main():
                     os.remove(dst_path) # removes that zip file 
         print("-----------------------------------")
 # end of main() ---------------------------------------------------
+
+
+# ------------------------------------
+window = Tk()
+window.title('Sort Assignment Submissions')
+
+
+classlistFile = StringVar()
+assignmentFolder = StringVar()
+submissionFolder = StringVar()
+
+
+
+# define button click functions
+def selectAssignmentFolder():
+    dir = filedialog.askdirectory()
+    assignmentFolder.set(dir)
+    
+def selectClasslist():
+    file = filedialog.askopenfilename()
+    classlistFile.set(file)
+
+def selectSubmissionFolder():
+    dir = filedialog.askdirectory()
+    submissionFolder.set(dir)
+    
+def sortSubmissions():
+    os.chdir(assignmentFolder.get())
+    
+    # reads in the classlist
+    classlist = []
+    with open(classlistFile.get(), mode = 'r') as file:
+        csvFile = csv.reader(file)
+        for line in csvFile:
+            classlist.append((line[0] + line[1]).strip(" "))
+            
+    # gets the directories for assignment folder and submissions 
+    os.chdir(submissionFolder.get())
+
+    # get all of the files in the submissions folder
+    fileList = os.listdir(os.getcwd())
+    
+
+    # goes through each student, and find's all the files they submitted
+    for student in classlist:
+        # sets source and destination for moving files
+        source = os.getcwd()
+        destination = assignmentFolder.get() + f"/{student}"
+        
+        print(f"{student}'s Submission Contents:")
+        for file in fileList:
+            if (file.find(student) != -1):
+
+                # moves the file from 
+                src_path = os.path.join(source, file)
+                dst_path = os.path.join(destination, file)
+                shutil.move(src_path, dst_path)
+                
+                print(file)
+                
+                # now extracts it if its a zip
+                if(file.endswith(".zip")):
+                    zip = ZipFile(dst_path)
+                    zip.extractall(destination)
+                    zip.close()
+                    os.remove(dst_path) # removes that zip file 
+        print("-----------------------------------")
+
+    
+    
+# creating all the display elements
+label_assignmentFolder = Label(text="Assignment Folder: ")
+entry_assignmentFolder = Entry(window, textvariable = assignmentFolder)
+button_explore1 = Button(window,
+                        text = "Browse Folders",
+                        command = selectAssignmentFolder)
+
+label_classlist = Label(text="Classlist: ")
+entry_classlistFile = Entry(window, textvariable = classlistFile)
+button_explore2 = Button(window,
+                        text = "Browse Files",
+                        command = selectClasslist)
+
+label_submissionsFolder = Label(text="Submissions Folder: ")
+entry_submissionsFolder = Entry(window, textvariable = submissionFolder)
+button_explore3 = Button(window,
+                        text = "Browse Folders",
+                        command = selectSubmissionFolder)
+
+button_sortSubmissions = Button(window,
+                                text = "Sort Submissions",
+                                command = sortSubmissions)
+
+
+
+# Sets the elements to the basic grid
+label_assignmentFolder.grid(column=1, row=1)
+entry_assignmentFolder.grid(column=2, row=1)
+button_explore1.grid(column = 3, row = 1)
+
+label_classlist.grid(column=1, row=2)
+entry_classlistFile.grid(column=2, row=2)
+button_explore2.grid(column = 3, row = 2)
+
+label_submissionsFolder.grid(column=1, row=3)
+entry_submissionsFolder.grid(column=2, row=3)
+button_explore3.grid(column = 3, row = 3)
+
+
+button_sortSubmissions.grid(column=4, row=5)
+
+
+# -----------
+window.mainloop()   
 
 
 
